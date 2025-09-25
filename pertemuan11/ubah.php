9708b82e5b9c8f4fe408bb5f3dcd8ea3388d9982
naufalbintang <naufalbintangpradana@gmail.com>
2025-09-25 08:40:02 +0700

    pertemuan11 - ubah.php

---
+++ pertemuan11/ubah.php
@@ -0,0 +1,67 @@
+<?php
+    include "functions.php";
+
+    // ambil id di url
+    $id = $_GET["id"];
+    // query data mahasiswa berdasarkan id
+    $mhs = query("SELECT * FROM mahasiswa WHERE id=$id")[0];
+    
+    // cek apakah tombol submit sudah ditekan atau belum
+    if(isset($_POST["submit"])){
+        // cek apakah data berhasil diubah atau tidak
+        if(ubah($_POST) > 0){
+            echo "<script>
+                    alert('data berhasil diubah');
+                    document.location.href = 'index.php';
+                </script>";
+        }else{
+            echo "<script>
+                    alert('data gagal diubah');
+                    document.location.href = 'index.php';
+                </script>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Ubah Data Mahasiswa</h1>
+    <a href="index.php">Kembali Ke Halaman Utama</a>
+    <br>
+    <br>
+    <form action="" method="post">
+        <ul>
+            <input type="text" name="id" value=<?= $mhs["id"]?> hidden>
+            <li>
+                <label for="nama">Nama: </label>
+                <input type="text" name="nama" id="nama" required value="<?=$mhs["nama"]?>">
+            </li>
+            <li>
+                <label for="nim">NIM: </label>
+                <input type="text" name="nim" id="nim" required value="<?=$mhs["nim"]?>">
+            </li>
+            <li>
+                <label for="email">Email: </label>
+                <input type="text" name="email" id="email" required value="<?=$mhs["email"]?>">
+            </li>
+            <li>
+                <label for="prodi">Prodi: </label>
+                <input type="text" name="prodi" id="prodi" required value="<?=$mhs["prodi"]?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar: </label>
+                <input type="text" name="gambar" id="gambar" required value="<?=$mhs["gambar"]?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
